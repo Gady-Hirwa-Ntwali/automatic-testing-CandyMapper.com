bc8e36cb69af902cbc10232cc569e9214826183b
Gady-Hirwa-Ntwali <hirwagad767@gmail.com>
2025-12-08 13:59:36 +0200

    refine the more drop down test

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -2,6 +2,8 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -17,6 +19,8 @@ public class HomePage {
         this.driver = driver;
     }
     public void clickRemovePopUp(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(removePop));
         driver.findElement(removePop).click();
     }
     public LoginPage clickJoinUsLink(){
